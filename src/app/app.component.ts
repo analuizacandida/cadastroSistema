04cd8aa3fbb0fd701f41c76989082cb933f79b87
analuizacandida <analuizacandidavc@gmail.com>
2025-08-13 16:58:14 -0300

    criar formulario e capturar valores dos campos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    RouterLink,
+    MatIconModule,
+    MatFormFieldModule,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';	
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { Cliente } from './cliente';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+  cliente: Cliente = Cliente.newCliente();
+
+  salvar() {
+    console.log('Cliente salvo:', this.cliente);
+   
+  }
+
+}
